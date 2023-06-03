6e54a601ab9260ecea2081ace980365f29e48253
nikhilbanga <nikhil@banga.com>
2023-06-03 18:53:42 +0530

    readme done

--- Components/ContactsScreen.js
+++ Components/ContactsScreen.js
@@ -1,116 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { View, Text, FlatList, StyleSheet, Modal } from "react-native";
-// import * as Contacts from "expo-contacts";
-// import ContactCard from "./ContactCard";
-// import SearchInput from "./SearchInput";
-// import ModalBox from "./ModalBox";
-// import Heading from "./Heading";
-
-// const ContactsScreen = () => {
-//   const [contacts, setContacts] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [filteredContacts, setFilteredContacts] = useState([]);
-//   const [selectedContact, setSelectedContact] = useState(null);
-//   const [modalVisible, setModalVisible] = useState(false);
-//   const [timeoutId, setTimeoutId] = useState(null);
-
-//   useEffect(() => {
-//     requestContactsPermission();
-//   }, []);
-
-//   useEffect(() => {
-//     filterContacts();
-//   }, [contacts, searchQuery]);
-//   const requestContactsPermission = async () => {
-//     const { status } = await Contacts.requestPermissionsAsync();
-
-//     if (status === "granted") {
-//       loadContacts();
-//     }
-//   };
-
-//   const loadContacts = async () => {
-//     const { data } = await Contacts.getContactsAsync({
-//       fields: [
-//         Contacts.Fields.FirstName,
-//         Contacts.Fields.LastName,
-//         Contacts.Fields.PhoneNumbers,
-//       ],
-//     });
-
-//     if (data.length > 0) {
-//       setContacts(data);
-//     }
-//   };
-
-//   const filterContacts = () => {
-//     const lowerCaseQuery = searchQuery.toLowerCase();
-//     const filtered = contacts.filter((contact) => {
-//       const fullName = `${contact.firstName} ${contact.lastName}`.toLowerCase();
-//       return fullName.includes(lowerCaseQuery);
-//     });
-//     setFilteredContacts(filtered);
-//   };
-
-//   const handleSearch = (query) => {
-//     setSearchQuery(query);
-//   };
-
-//   const openContactDetails = (contact) => {
-//     setSelectedContact(contact);
-//     setModalVisible(true);
-
-//     const timeout = setTimeout(() => {
-//       setModalVisible(false);
-//     }, 5000);
-
-//     setTimeoutId(timeout);
-//   };
-
-//   const closeModal = () => {
-//     setModalVisible(false);
-//     clearTimeout(timeoutId);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Heading />
-
-//       <SearchInput value={searchQuery} onChangeText={handleSearch} />
-
-//       {filteredContacts && filteredContacts.length > 0 ? (
-//         <FlatList
-//           data={filteredContacts}
-//           renderItem={({ item }) => (
-//             <ContactCard item={item} onPress={openContactDetails} />
-//           )}
-//           keyExtractor={(item) => item.id}
-//         />
-//       ) : (
-//         <Text style={styles.loadingText}>Loading contacts...</Text>
-//       )}
-
-//       <Modal visible={modalVisible} animationType="fade" transparent>
-//         <ModalBox closeModal={closeModal} selectedContact={selectedContact} />
-//       </Modal>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 16,
-//     backgroundColor: "#1B262C",
-//   },
-//   loadingText: {
-//     fontSize: 16,
-//     marginTop: 16,
-//     color: "#BBE1FA",
-//   },
-// });
-
-// export default ContactsScreen;
 import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, StyleSheet, Modal } from "react-native";
 import * as Contacts from "expo-contacts";
@@ -120,11 +7,11 @@ import ModalBox from "./ModalBox";
 import Heading from "./Heading";
 
 const ContactsScreen = () => {
-  const [contacts, setContacts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredContacts, setFilteredContacts] = useState([]);
-  const [selectedContact, setSelectedContact] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
+  const [contactsList, setContactsList] = useState([]);
+  const [searchQueryText, setSearchQueryText] = useState("");
+  const [filteredContactsList, setFilteredContactsList] = useState([]);
+  const [selectedContactData, setSelectedContactData] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     requestContactsPermission();
@@ -132,7 +19,7 @@ const ContactsScreen = () => {
 
   useEffect(() => {
     filterContacts();
-  }, [contacts, searchQuery]);
+  }, [contactsList, searchQueryText]);
 
   const requestContactsPermission = async () => {
     const { status } = await Contacts.requestPermissionsAsync();
@@ -152,41 +39,41 @@ const ContactsScreen = () => {
     });
 
     if (data.length > 0) {
-      setContacts(data);
+      setContactsList(data);
     }
   };
 
   const filterContacts = () => {
-    const lowerCaseQuery = searchQuery.toLowerCase();
-    const filtered = contacts.filter((contact) => {
+    const lowerCaseQuery = searchQueryText.toLowerCase();
+    const filtered = contactsList.filter((contact) => {
       const fullName = `${contact.firstName} ${contact.lastName}`.toLowerCase();
       return fullName.includes(lowerCaseQuery);
     });
-    setFilteredContacts(filtered);
+    setFilteredContactsList(filtered);
   };
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
+  const handleSearchQuery = (query) => {
+    setSearchQueryText(query);
   };
 
   const openContactDetails = (contact) => {
-    setSelectedContact(contact);
-    setModalVisible(true);
+    setSelectedContactData(contact);
+    setIsModalVisible(true);
   };
 
   const closeModal = () => {
-    setModalVisible(false);
+    setIsModalVisible(false);
   };
 
   return (
     <View style={styles.container}>
       <Heading />
 
-      <SearchInput value={searchQuery} onChangeText={handleSearch} />
+      <SearchInput value={searchQueryText} onChangeText={handleSearchQuery} />
 
-      {filteredContacts && filteredContacts.length > 0 ? (
+      {filteredContactsList && filteredContactsList.length > 0 ? (
         <FlatList
-          data={filteredContacts}
+          data={filteredContactsList}
           renderItem={({ item }) => (
             <ContactCard item={item} onPress={openContactDetails} />
           )}
@@ -197,12 +84,15 @@ const ContactsScreen = () => {
       )}
 
       <Modal
-        visible={modalVisible}
+        visible={isModalVisible}
         animationType="fade"
         transparent
         onRequestClose={closeModal}
       >
-        <ModalBox closeModal={closeModal} selectedContact={selectedContact} />
+        <ModalBox
+          closeModal={closeModal}
+          selectedContact={selectedContactData}
+        />
       </Modal>
     </View>
   );
